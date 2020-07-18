311742c1f39c332db72538a409ba528658d06bc8
Duncan Leo <hello@duncanleo.me>
2020-07-18 12:01:05 +0800

    chore: minor header styling

--- src/frontend/components/Header.tsx
+++ src/frontend/components/Header.tsx
@@ -6,6 +6,7 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding: 16px 0;
 `;
 
 const Title = styled(Link)`
@@ -14,11 +15,14 @@ const Title = styled(Link)`
   font-style: italic;
   font-weight: 700;
   font-size: 4em;
+  margin-bottom: 16px;
 `;
 
 const PageLink = styled(Link)`
   text-decoration: none;
   color: black;
+  padding: 4px 8px;
+  border-radius: 3px;
 
   &:hover {
     background-color: #444;
@@ -28,8 +32,9 @@ const PageLink = styled(Link)`
 `;
 
 const PageLinksContainer = styled.div`
-  display: flex;
-  flex-direction: row;
+  display: grid;
+  grid-auto-flow: column;
+  column-gap: 8px;
 `;
 
 const Header: React.FC = function () {
